9553509ebad145f0e9326e18ecafacbc6c2502f0
Mateus <mateusbarbozamarques99@gmail.com>
2022-02-17 22:53:21 -0300

    Update ex003.py

--- Exercicios/ex003.py
+++ Exercicios/ex003.py
@@ -1,7 +1,7 @@
 n1 =int(input('Primeiro Número:'))
-n2 = int(input('Segundo numero:'))
+n2 = int(input('Segundo Número:'))
 s = n1 + n2
-print('A soma entre {} e {} vale {}'.format(n1,n2, s))
+print('A soma entre {} e {} vale {}'.format(n1, n2, s))
 
 
 
