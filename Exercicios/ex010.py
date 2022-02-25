053c05722c46aebbfea014e97d6f56b2e93f0a4d
Mateus <mateusbarbozamarques99@gmail.com>
2022-02-25 18:29:31 -0300

    commit

--- Exercicios/ex010.py
+++ Exercicios/ex010.py
@@ -2,7 +2,3 @@ real = float(input('Quanto você tem na Carteira? R$'))
 dolar = real / 5.11
 euro = real / 5.76
 print('Com R${:.2f}  você pode comprar U$${:.2f} e €{:.2f} '.format(real, dolar, euro))
-
-
-
-
--- Exercicios/ex013.py
+++ Exercicios/ex013.py
@@ -1,3 +1,3 @@
 salário = float(input('Quanto você ganha na Empresa: R$'))
 reajuste = salário + (salário * 15 / 100)
-print('Seu salário é de R${:.2f} com reajuste de 15%, você passa a ganhar R${:.2f}'.format(salário, reajuste))
+print('Seu salário é de R${:.3f} com reajuste de 15%, você passa a ganhar R${:.3f}'.format(salário, reajuste))
