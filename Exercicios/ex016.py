55fb1baaf259c0c74910e5e43f24bd36ac370a87
Mateus <mateusbarbozamarques99@gmail.com>
2022-03-01 23:41:29 -0300

    commit

--- Exercicios/ex016.py
+++ Exercicios/ex016.py
@@ -1,1 +1,6 @@
+'''from math import trunc
+num = float(input('Digite um número:'))
+print('O valor Digitado foi {} ea sua porção inteira é {}'.format(num, trunc(num)))'''
 
+num = float(input('Digite um número:'))
+print('O valor Digitado foi {} ea sua porção inteira é {}'.format(num, int(num)))
