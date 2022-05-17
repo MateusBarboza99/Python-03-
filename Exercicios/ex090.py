4503738ad895ac230062fcfa9f57b3c8bc58e9b5
Mateus <mateusbarbozamarques99@gmail.com>
2022-05-16 23:15:25 -0300

    commit

---
+++ Exercicios/ex090.py
@@ -0,0 +1,14 @@
+aluno = dict()
+aluno['nome'] = str(input('Nome: '))
+aluno['média'] = float(input(f'Média de {aluno["nome"]}: '))
+if aluno['média'] >= 7:
+    aluno['Situação'] = 'Aprovado'
+elif 5 <= aluno['média'] < 7:
+    aluno['Situação'] = 'Recuperação'
+else:
+    aluno['Situação'] = 'Reprovado'
+print('-=' * 30)
+for k, v in aluno.items():
+    print(f'  - {k} é igual a {v}')
+
+
